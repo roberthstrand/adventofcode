9d1bf4133201fc5fa0e176ef13e3deee272cff3a
Roberth Strand <me@robstr.dev>
2022-12-01 14:41:06 +0100

    feat: solutions with input from aoc
    Signed-off-by: Roberth Strand <me@robstr.dev>

--- 2022/01/golang/main.go
+++ 2022/01/golang/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"sort"
+	"strconv"
+	"strings"
 )
 
 type Elves struct {
@@ -12,26 +15,38 @@ type Elves struct {
 
 type byCalories []Elves
 
-var originalElvesList = []Elves{
-	{number: 1, calories: []int{1000, 2000, 3000}},
-	{number: 2, calories: []int{4000}},
-	{number: 3, calories: []int{5000, 6000}},
-	{number: 4, calories: []int{7000, 8000, 9000}},
-	{number: 5, calories: []int{10000}},
-}
+var (
+	originalElvesList = []Elves{
+		{number: 1, calories: []int{1000, 2000, 3000}},
+		{number: 2, calories: []int{4000}},
+		{number: 3, calories: []int{5000, 6000}},
+		{number: 4, calories: []int{7000, 8000, 9000}},
+		{number: 5, calories: []int{10000}},
+	}
+)
 
 func (e byCalories) Len() int {
 	return len(e)
 }
 
 func main() {
+	input, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	inputString := string(input)
+	inputSplit := strings.Split((strings.Replace(inputString, "\n", ",", -1)), ",,")
+
 	allElves := []Elves{}
-	for _, elves := range originalElvesList {
+
+	for elves, calories := range inputSplit {
 		var totalCal int
-		for _, calories := range elves.calories {
-			totalCal += calories
+		for _, calories := range strings.Split(calories, ",") {
+			cal, _ := strconv.Atoi(calories)
+			totalCal += cal
 		}
-		allElves = append(allElves, Elves{number: elves.number, calories: []int{totalCal}})
+		allElves = append(allElves, Elves{number: elves, calories: []int{totalCal}})
 	}
 
 	sort.Slice(allElves, func(i, j int) bool {
